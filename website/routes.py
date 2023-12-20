8a16202db4e0fd5099663b3969f13b60033f6782
Nikitha2404 <hebbar.nikitha@gmail.com>
2023-12-20 21:21:03 +0530

    dynamic routes

--- website/routes.py
+++ website/routes.py
@@ -8,6 +8,10 @@ def db_conn():
     return conn
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/courses')
 def course():
 
     conn = db_conn()
@@ -19,23 +23,30 @@ def course():
 
     return render_template('course.html',data=data)
 
-@app.route('/11')
-def syllogisum():
+@app.route('/courses/<int:CourseId>/lessons')
+def lessonList(CourseId):
      
     conn = db_conn()
     cur = conn.cursor()
 
-    select_query = f'''SELECT * FROM lesson where course_id=11;'''
+    select_query = f'''SELECT * FROM lesson where course_id={CourseId};'''
     cur.execute(select_query)
     data = cur.fetchall()
 
-    return render_template('lesson.html',data=data)
+    return render_template('lessons.html',data=data)
 
+@app.route('/courses/<int:CourseId>/lessons/<int:LessonId>')
+def getMaterial(CourseId, LessonId):
 
-@app.route('/111')
-def syllogisuml1():
-     
-    return render_template('material.html')
+    conn = db_conn()
+    cur = conn.cursor()
+
+    select_query = f'''SELECT * FROM lesson where lesson_id={LessonId} AND course_id={CourseId};'''
+    cur.execute(select_query)
+    data = cur.fetchall()
+    print(data)
+
+    return render_template('material.html',data=data)
 
 @app.route('/quiz')
 def quiz():
